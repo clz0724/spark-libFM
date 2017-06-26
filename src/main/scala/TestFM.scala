eba6fab029473a05bc744cc13ae28991f12993c3
chenlongzhen <longzhen.chen@foxmail.com>
2017-06-26 09:18:33 +0800

    process libsvmfile

--- src/main/scala/TestFM.scala
+++ src/main/scala/TestFM.scala
@@ -10,13 +10,40 @@ import org.apache.spark.mllib.util.MLUtils
 
 
 object TestFM extends App {
+  def indiceChange(sc: SparkContext,path_in :String,path_out:String): Unit ={
+    val data = sc.textFile(path_in)
+    val train = data.map{
+      line=>
+        val segs: Array[String] = line.split('\t')
+        val label = if(segs(0) == "1") "1" else "-1"
+        val features = segs.drop(1)
+        val features_process = features.map{
+          elem =>
+            val index = elem.split(":")(0).toInt
+            val value = elem.split(":")(1)
+            val new_index = index + 1
+            index.toString + value
+        }
+        val line_arr = label +: features_process
+        line_arr.mkString(" ")
+    }
+    train.saveAsTextFile(path_out)
+
+  }
+
 
   override def main(args: Array[String]): Unit = {
 
-    val sc = new SparkContext(new SparkConf().setAppName("TESTFM"))
+    val sc: SparkContext = new SparkContext(new SparkConf().setAppName("TESTFM"))
+
+    val path_in = "/team/ad_wajue/dw/rec_ml_test/rec_ml_test/model_dataSet/training"
+    val path_out = "/team/ad_wajue/dw/rec_ml_test/rec_ml_test/model_dataSet/training_processed"
+
+    // process lines
+    indiceChange(sc,path_in,path_out)
 
     //    "hdfs://ns1/whale-tmp/url_combined"
-    val training = MLUtils.loadLibSVMFile(sc, "hdfs://ns1/whale-tmp/url_combined").cache()
+    val training = MLUtils.loadLibSVMFile(sc, path_out).cache()
 
     //    val task = args(1).toInt
     //    val numIterations = args(2).toInt
